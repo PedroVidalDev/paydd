94a2942028af0957b2927df489a3776943a82332
PedroVidalDev <pedrohvidals@gmail.com>
2024-11-18 22:52:59 -0300

    feat: create debt screen component

---
+++ src/pages/Debt/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles"
+
+export const Debt = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
---
+++ src/pages/Debt/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    flex: 1;
+
+    background-color: ${({theme}) => theme.COLORS.SECUNDARY_LIGHT};
+`

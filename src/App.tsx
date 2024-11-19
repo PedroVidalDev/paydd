2d32c1ae9c933b0b2ba556ed5661c49b051f9824
PedroVidalDev <pedrohvidals@gmail.com>
2024-11-18 22:53:26 -0300

    feat: add menu and screen component in app for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { Debt } from 'pages/Debt';
+import { Menu } from 'components/Menu';
+
+import { theme } from 'themes';
+import { ThemeProvider } from 'styled-components';
+import { MenuType } from 'components/Menu/types';
 
 function App() {
   return (
-    <>
-    </>
+    <ThemeProvider theme={theme}>
+      <Debt />
+      <Menu type={MenuType.CREDIT} />
+    </ThemeProvider>
   );
 }
 

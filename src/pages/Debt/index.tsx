3a38b22c56ba839f57e9bc8d04bde7fbd079a44b
PedroVidalDev <pedrohvidals@gmail.com>
2024-11-24 09:07:57 -0300

    feat: create name filter

--- src/pages/Debt/index.tsx
+++ src/pages/Debt/index.tsx
@@ -1,16 +1,29 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import { Controller, useForm } from "react-hook-form";
+import { useCallback, useEffect, useState } from "react";
 
 import { Input } from "components/Input";
 import { Button } from "components/Button";
 import { RegisterContainer } from "components/RegisterContainer"
 
-import { Container, ContainerInputs, ContainerTitle } from "./styles"
+import { REGEX } from "constants/regex";
+import { SearchFilterData } from "./types";
 import { DebtCompleteData } from "dto/DebtDTO";
+import { Container, ContainerInputs, ContainerTitle } from "./styles"
 
 export const Debt = () => {
 
     const [debtList, setDebtList] = useState<DebtCompleteData[]>([]);
+    
+    const navigate = useNavigate()
+
+    const { control, watch, formState: {errors} } = useForm<SearchFilterData>({
+        defaultValues: {
+            name: ''
+        }
+    })
+
+    const watchName = watch('name');
 
     const getAllDebts = () => {
         let storedDebtList = localStorage.getItem('debtList');
@@ -24,11 +37,31 @@ export const Debt = () => {
         console.log(debtList)
     }
 
-    const navigate = useNavigate()
+    const getAllDebtsFiltered = useCallback(() => {
+        let storedDebtList = localStorage.getItem('debtList');
+
+        if (storedDebtList == null) {
+            localStorage.setItem('debtList', JSON.stringify([]));
+            storedDebtList = '[]';
+        }
+
+        const parsedDebtList = JSON.parse(storedDebtList) as DebtCompleteData[];
+        const filteredDebtList = parsedDebtList.filter(debt => debt.name.includes(watchName));
+
+        setDebtList(filteredDebtList);
+    }, [watchName])
 
     const handleViewCreate = () => {
         navigate('/debt/new')
     }
+
+    useEffect(() => {
+        if(watchName) {
+            getAllDebtsFiltered()
+        } else {
+            getAllDebts()
+        }
+    }, [watchName, getAllDebtsFiltered, getAllDebts])
 
     useEffect(() => {
         getAllDebts()
@@ -38,7 +71,26 @@ export const Debt = () => {
         <Container>
             <ContainerTitle> Débitos </ContainerTitle>
             <ContainerInputs>
-                <Input hasError={false} placeholder="Digite o nome aqui..." />
+            <Controller 
+                    control={control}
+                    name="name"
+                    rules={{
+                        required: "Campo necessário",    
+                        pattern: {
+                            value: REGEX.onlyString,
+                            message: "Campo deve ser um texto"
+                        }
+                    }}
+                    render={({ field: {onChange, value} }) => (
+                        <Input
+                            hasError={!!errors?.name}
+                            errorMessage={errors?.name?.message}
+                            onChange={onChange}
+                            value={value}
+                            placeholder="Digite o nome aqui..."
+                        />
+                    )}
+                />                
                 <Button onClick={handleViewCreate} text="+" height={40} width={40}/>
                 <Button text="H" height={40} width={40} />
             </ContainerInputs>
